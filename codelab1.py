75b27512cad1da61c88780f53606b42a30c37421
eileenafabiolina <148841046+eileenafabiolina@users.noreply.github.com>
2023-12-06 20:55:47 -0600

    Update codelab1.py

--- codelab1.py
+++ codelab1.py
@@ -1,1 +1,16 @@
+entero = 42
+flotante = 3.14
+cadena = "Hola, soy una cadena"
+booleano = True
 
+resultado_concatenacion = cadena + str(entero) + str(flotante) + str(booleano)
+
+limite_enteros = "El límite de los enteros en Python es " + str(pow(2, 63) - 1)
+
+n = entero
+suma_pares = n * (n + 1)
+
+print("Resultado de la concatenación:", resultado_concatenacion)
+print(limite_enteros)
+print(limite_flotantes)
+print("Resultado de la suma de los primeros", n, "números pares:", suma_pares)
